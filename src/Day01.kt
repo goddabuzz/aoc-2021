73b40b1a26e8afdb0481b6c48c7a43d1f8e55389
Daniel Kloosterman <buz.i286@gmail.com>
2021-12-01 08:37:48 +0100

    Day01.1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,6 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(input.filterIndexed {
+        index, s -> index != 0 && input[index - 1].toInt() < s.toInt()
+    }.size)
 }
