66fc17f75701f14f8592f44fce604909f24f1953
Daniel Kloosterman <buz.i286@gmail.com>
2021-12-01 11:50:00 +0100

    Optimized

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,14 +1,15 @@
 fun main() {
 
-    fun part1(input: List<Int>): Int = input.filterWithPrev { prev, current -> prev < current }.size
+    fun part1(input: List<Int>): Int = input.zipWithNext { a, b -> a < b }.filter { it }.size
 
-    fun part2(input: List<Int>): Int = input.mapIndexedNotNull { index, i ->
+    fun part2(input: List<Int>): Int = part1(input.mapIndexedNotNull { index, i ->
         if (index < input.size - 2) {
             i + input[index + 1] + input[index + 2]
         } else null
-    }.filterWithPrev { prev, current -> prev < current }.size
+    })
 
     val input = readInput("Day01").map { it.toInt() }
+
     result(part1(input), 1342)
     result(part2(input), 1378)
 }
