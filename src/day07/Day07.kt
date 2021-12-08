512b7773ccf9586a21d534d7c83c98fec0bb25e7
Daniel Kloosterman <buz.i286@gmail.com>
2021-12-08 13:55:01 +0100

    d8p1

--- src/day07/Day07.kt
+++ src/day07/Day07.kt
@@ -6,12 +6,11 @@ import kotlin.math.abs
 
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        val mov = input[0].split(",").filter { it.isNotBlank() }.map { it.toInt() }
+    fun part1(input: List<Int>): Int {
         var min = Integer.MAX_VALUE
-        for (i in 0..mov.size) {
+        for (i in 0..input.size) {
             var c = 0
-            mov.forEach {
+            input.forEach {
                 c += abs(i - it);
                 if (c > min) return@forEach
             }
@@ -20,25 +19,25 @@ fun main() {
         return min
     }
 
-    fun part2(input: List<String>): Int {
-        val mov = input[0].split(",").filter { it.isNotBlank() }.map { it.toInt() }
+    fun part2(input: List<Int>): Int {
         var min = Integer.MAX_VALUE
-        for (i in 0..mov.size) {
+        for (i in 0..input.size) {
             var c = 0
-            mov.forEach {
-                c += abs(i - it) + (abs(i - it) * abs(i - it))
+            input.forEach {
+                val n = abs(i - it);
+                c += n + n * n
                 if (c > min) return@forEach
             }
             min = if (c < min) c else min
         }
-        return min/2
+        return min / 2
     }
 
-    val testInput = readInput("day07/Day07_test")
+    val testInput = readInput("day07/Day07_test")[0].split(",").map { it.toInt() }
     result(part1(testInput), 37)
     result(part2(testInput), 168)
 
-    val input = readInput("day07/Day07")
+    val input = readInput("day07/Day07")[0].split(",").map { it.toInt() }
     result(part1(input), 340987)
     result(part2(input), 96987874)
 }
---
+++ src/day08/Day08.kt
@@ -0,0 +1,20 @@
+package day08
+
+import readInput
+import result
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return input.map { it.split(" | ").last() }
+            .flatMap { it.split(" ") }
+            .count { it.length in listOf(2, 3, 4, 7) }
+    }
+
+
+    val testInput = readInput("day08/Day08_test")
+    result(part1(testInput), 26)
+
+    val input = readInput("day08/Day08")
+    result(part1(input), 294)
+}
