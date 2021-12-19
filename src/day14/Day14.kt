18e3c5674e2f23af47060d8d762f014b8f1cc193
Daniel Kloosterman <buz.i286@gmail.com>
2021-12-19 22:06:01 +0100

    d142

--- src/day14/Day14.kt
+++ src/day14/Day14.kt
@@ -2,20 +2,24 @@ package day14
 
 import readInput
 import result
+import resultLong
 
 fun main() {
     result(part1(readInput("Day14/Day14_test")), 1588)
     result(part1(readInput("Day14/Day14")), 2621)
+
+    resultLong(part2(readInput("Day14/Day14_test")), 2188189693529)
+    resultLong(part2(readInput("Day14/Day14")), 2843834241366)
 }
 
 fun part1(input: List<String>): Int {
 
     var template = input.first()
     val last = template.last()
-    val rules = input.filter { "->" in it }.map { it.split(" -> ") }
+    val rules = input.filter { "->" in it }
+        .map { it.split(" -> ") }
 
-    for (i in 0..9) {
-
+    repeat(10) {
         template = template.windowed(2).map { seq ->
             rules.forEach { rule ->
                 if (rule[0][0] == seq[0] && rule[0][1] == seq[1]) {
@@ -24,9 +28,48 @@ fun part1(input: List<String>): Int {
             }
             seq[0]
         }.joinToString("") + last
-        println(template)
     }
 
     val counts = template.groupBy { it }.map { entry -> entry.value.size }
     return counts.maxOf { it } - counts.minOf { it }
+}
+
+fun part2(input: List<String>): Long {
+
+    val pairs = mutableMapOf<String, Long>()
+    val template = input[0]
+    val rules = input.filter { " -> " in it }
+        .map { it.split(" -> ") }
+        .groupBy { it[0] }
+        .mapValues { it.value[0][1] }
+
+    template.windowed(2).forEach {
+        pairs[it] = pairs.getOrDefault(it, 0) + 1
+    }
+
+    repeat(40) {
+        val newPairs = mutableMapOf<String, Long>()
+        for ((key, value) in pairs) {
+            if (key in rules) {
+                val leftPair = key[0] + rules[key]!!
+                val rightPair = rules[key]!! + key[1]
+
+                newPairs[leftPair] = newPairs.getOrDefault(leftPair, 0) + value
+                newPairs[rightPair] = newPairs.getOrDefault(rightPair, 0) + value
+
+                pairs[key] = 0
+            }
+        }
+
+        for ((key, value) in newPairs) {
+            pairs[key] = pairs.getOrDefault(key, 0) + value
+        }
+    }
+
+    val elemFreq = mutableMapOf<Char, Long>()
+    elemFreq[template.last()] = 1
+    pairs.forEach { (key, value) ->
+        elemFreq[key[0]] = elemFreq.getOrDefault(key[0], 0) + value
+    }
+    return elemFreq.maxOf { it.value } - elemFreq.minOf { it.value }
 }
