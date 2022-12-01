13d255cd7656759683e38f8737df6abb045780f7
Daniel Kloosterman <buz.i286@gmail.com>
2022-12-01 11:53:28 +0100

    geoptimaliseerd

--- src/main/kotlin/aoc2022/day01/Day01.kt
+++ src/main/kotlin/aoc2022/day01/Day01.kt
@@ -4,37 +4,25 @@ import readInput
 import result
 
 fun main() {
-    val t1 = readInput(2022,"day01/Day01_test")
-    val p1 = readInput(2022,"day01/Day01");
+    val t1 = readInput(2022, "day01/Day01_test")
+    val p1 = readInput(2022, "day01/Day01");
 
-    result(part1(splitElfs(t1)), 24000)
-    result(part1(splitElfs(p1)), 67658)
-    result(part2(splitElfs(t1)), 45000)
-    result(part2(splitElfs(p1)), 45000)
+    result(part1(chunk(t1)), 24000)
+    result(part1(chunk(p1)), 67658)
+    result(part2(chunk(t1)), 45000)
+    result(part2(chunk(p1)), 200158)
 }
 
-fun part2(elfs: List<List<Int>>): Int {
-    val elfCalorie = elfs.map { it.sumOf { i: Int -> i } }
-    return elfCalorie.sorted().takeLast(3).sum()
-}
+fun part2(elfs: List<List<Int>>): Int = elfs.map { it.sumOf { i: Int -> i } }.sorted().takeLast(3).sum()
 
-fun part1(elfs: List<List<Int>>): Int {
-    return elfs.maxOf { ints: List<Int> -> ints.sumOf { i: Int -> i } }
-}
+fun part1(elfs: List<List<Int>>): Int = elfs.maxOf { ints: List<Int> -> ints.sumOf { i: Int -> i } }
 
-fun splitElfs(p1: List<String>): List<List<Int>> {
-    val elfs = mutableListOf<List<Int>>()
-    var elf = mutableListOf<Int>()
-    elfs.add(elf)
-    p1.forEach { s ->
-        run {
-            if (s.isEmpty()) {
-                elf = mutableListOf()
-                elfs.add(elf)
-            } else {
-                elf.add(s.toInt())
-            }
+fun chunk(lines: List<String>): List<List<Int>> =
+    lines.fold(mutableListOf(mutableListOf())) { chunks: MutableList<MutableList<Int>>, items ->
+        if (items.isBlank()) {
+            chunks.add(mutableListOf())
+        } else {
+            chunks.last().add(items.toInt())
         }
-    }
-    return elfs.toList()
-}
+        chunks
+    }.toList()
